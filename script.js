3dbd7de61172d33917512ac1ea939c097c4a564d
Caíque Fernando <caique_flr@hotmail.com>
2023-01-07 00:37:42 -0300

    h1 update

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const ts = '1672962897745'
 const apikey = 'accc71baf7ca566998e165f2c464d597'
 const md5 = 'cf83c6980e274a13eb7cfffdb9d8be54'
 
-fetch(`https://gateway.marvel.com:443/v1/public/characters?ts=${ts}&apikey=${apikey}&hash=${md5}`)
+fetch(`https://gateway.marvel.com:443/v1/public/characters?limit=100&ts=${ts}&apikey=${apikey}&hash=${md5}`)
   .then(response => response.json())
   .then(resp => {
     const divChar = document.querySelector('#characters')
-    
+
     resp.data.results.forEach(element => {
       const imgChar = `${element.thumbnail.path}.${element.thumbnail.extension}`
       const nameChar = element.name
